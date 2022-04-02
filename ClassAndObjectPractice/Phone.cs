e333feafe457ef06d7c6ba53cfff62084a013e4f
Zak Woodward <Zak Woodward@ZAKS-LAPTOP>
2022-04-02 16:40:56 +0100

    Add project files.

---
+++ ClassAndObjectPractice/Phone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjectPractice
+{
+    internal class Phone
+    {
+        public string colour;
+        public string brand;
+        public int storage;
+
+        // this is a constructor
+        public Phone(string aColour, string aBrand, int aStorage) //this is where the arguments go
+        {
+            this.colour = aColour;
+            this.brand = aBrand;
+            this.storage = aStorage;
+        }
+
+        // this is an object method
+        public bool HasEnoughStorage()
+        {
+            if (this.storage >= 32)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ClassAndObjectPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObjectPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating the object
+            Phone phone = new Phone("Gold", "iPhone", 22);
+
+            Console.WriteLine(phone.colour);
+            Console.WriteLine(phone.HasEnoughStorage()); // calling the method
+            Console.ReadLine();
+        }
+    }
+}
